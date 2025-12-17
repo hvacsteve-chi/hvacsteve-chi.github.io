c55a03ec656a8d9e4365daa3ceb46adb2fdbd877
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2025-12-17 10:47:08 -0600

    dec

--- web-developer/theme-toggle/js/global.js
+++ web-developer/theme-toggle/js/global.js
@@ -1,16 +1,24 @@
 const rootHTML = document.documentElement;
 console.log(rootHTML);
-const rootBODY = document.body;
-console.log(rootBODY);
 const rootHEAD = document.head;
 console.log(rootHEAD);
 const rootTITLE = document.title;
 console.log(rootTITLE);
-const rootAPP = document.querySelector("APP");
+const rootBODY = document.body;
+console.log(rootBODY);
+const rootAPP = document.querySelector("#SITEAPP");
 console.log(rootAPP);
-const DATENowSpan = document.getElementById("DATEnow");
-console.log(DATENowSpan);
-const NEWDate = new Date();
-console.log(NEWDate);
-DATENowSpan.innerHTML = NEWDate.getFullYear() + " ";
+const footerSpanDateNow = document.querySelector("#FOOTERSPANDATENOW");
+console.log(footerSpanDateNow);
+const newDATE = new Date();
+console.log(newDATE);
+const BRAND = {
+    name: "Faith Heating and Cooling Chicago",
+    shortName: "Faith HVAC",
+    address: "1709 W. Cornelia Ave, Chicago, IL 60657",
+    phone: 7734134100,
+    email: "faithheatingcooling@gmail.com"
+};
+console.log(BRAND);
+footerSpanDateNow.innerHTML = `&copy ${newDATE.getFullYear()} ${BRAND.shortName} - ${BRAND.address}`;
 
